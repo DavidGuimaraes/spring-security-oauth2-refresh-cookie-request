51abad560cd5fe9d35a9bc4903a03bffdeb46d85
David Guimaraes <david.ce@live.com>
2020-04-04 06:11:36 -0400

    01. Initializing project.

---
+++ oauth-refresh-jwt-cookie-request-demo/src/main/java/co/davidguimaraes/oauthrefreshjwtcookierequestdemo/OauthRefreshJwtCookieRequestDemoApplication.java
@@ -0,0 +1,13 @@
+package co.davidguimaraes.oauthrefreshjwtcookierequestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthRefreshJwtCookieRequestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthRefreshJwtCookieRequestDemoApplication.class, args);
+	}
+
+}
